93a30421a5e7a7655f59c6269135c4e9cd1283d6
Susanne Tünker <susanne.tuenker@hotmail.de>
2019-03-16 16:32:21 +0100

    log in work in progress

--- server/models/Designer.js
+++ server/models/Designer.js
@@ -5,7 +5,18 @@ const schema = mongoose.Schema({
   lastName: String,
   email: String,
   password: String,
-  passwordConfirm: String
+  passwordConfirm: String,
+  aboutYou: {
+    youinasentence: String,
+    position: String,
+    brand: String,
+    website: String,
+    adress: String,
+    city: String,
+    zip: Number,
+    country: String,
+    telephone: Number
+  }
 });
 
 module.exports = mongoose.model("Designer", schema);
--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -6,7 +6,8 @@ const bcrypt = require("bcryptjs");
 
 const Designer = require("../models/Designer");
 
-authRoutes.post("/designer", (req, res, next) => {
+// SIGN UP DESIGNER
+authRoutes.post("/signup/designer", (req, res, next) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
@@ -14,7 +15,6 @@ authRoutes.post("/designer", (req, res, next) => {
   const passwordConfirm = req.body.passwordConfirm;
 
   if (password != passwordConfirm) {
-    res.send("Wrong password");
     res.status(400).json({
       message: "Please type in correct password"
     });
@@ -23,7 +23,6 @@ authRoutes.post("/designer", (req, res, next) => {
 
   Designer.findOne({ email: email }).then(user => {
     if (user !== null) {
-      res.send("Your e-mail is already assigned to an account. Please Log in");
       res.status(400).json({
         message: "Your e-mail is already assigned to an account. Please Log in"
       });
@@ -42,15 +41,72 @@ authRoutes.post("/designer", (req, res, next) => {
 
     aNewUser.save(err => {
       if (err) {
-        res.send("something went wrong. Please try again");
         res
           .status(400)
           .json({ message: "Saving user to database went wrong." });
         return;
       } else {
-        res.send("all fine");
+        res.status(200).json({ message: "no Error" });
       }
+
+      req.login(aNewUser, err => {
+        if (err) {
+          res.status(500).json({ message: "Login after signup went bad." });
+          return;
+        }
+        res.status(200).json({ message: "user's info:" + aNewUser });
+      });
     });
   });
 });
+
+// LOG IN DESIGNER
+authRoutes.post("/login/designer", (req, res, next) => {
+  passport.authenticate("local", (err, theUser, failureDetails) => {
+    if (err) {
+      res
+        .status(500)
+        .json({ message: "Something went wrong authenticating user" });
+      return;
+    }
+
+    if (!theUser) {
+      // "failureDetails" contains the error messages
+      // from our logic in "LocalStrategy" { message: '...' }.
+      res.status(401).json(failureDetails);
+      return;
+    }
+
+    // save user in session
+    req.login(theUser, err => {
+      if (err) {
+        res.status(500).json({ message: "Session save went bad." });
+        return;
+      }
+
+      // We are now logged in (that's why we can also send req.user)
+      res.status(200).json(theUser);
+    });
+  })(req, res, next);
+});
+
+// LOG OUT DESIGNER
+authRoutes.post("/logout", (req, res, next) => {
+  req.logout();
+  res.status(200).json({ message: "Log out success!" });
+});
+
+// LOGGED IN DESIGNER
+authRoutes.get("/loggedin", (req, res, next) => {
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+    return;
+  }
+  res.status(403).json({ message: "Unauthorized" });
+});
+
+authRoutes.post("/signup/designer/aboutyou", (req, res) => {
+  console.log(req.body);
+});
+
 module.exports = authRoutes;
