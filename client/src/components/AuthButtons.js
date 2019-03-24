a0ad0b41f650e8207f3ebffa51447e4fec8011f9
Lara Petersen <laramarlenpetersen@gmail.com>
2019-03-24 14:00:15 +0100

    added authbutton change when logged in

--- client/src/components/AuthButtons.js
+++ client/src/components/AuthButtons.js
@@ -6,25 +6,53 @@ class AuthButtons extends Component {
   render() {
     // console.log(this.props.authButtonStatus);
     let dynamicbutton;
-    if (this.props.authButtonStatus === "signup") {
+    if (this.props.authButtonStatus === "loggedIn") {
       dynamicbutton = (
-        <Link to="/signup">
-          <button className="authButtonsSignUp">SIGN UP NOW</button>
-        </Link>
+        <div className="authButtons">
+          <Link to="/profile">
+            <img
+              src="/images/DmC_alarmicon.png"
+              alt="alarm icon"
+              width="25em"
+            />
+            <img
+              className="authButtonsAvatar"
+              src="/images/avatar_test.jpg"
+              alt="avatar"
+              width="45em"
+            />
+            <img
+              src="/images/DmC_arrowicon.png"
+              alt="alarm icon"
+              width="15em"
+            />
+          </Link>
+        </div>
+      );
+    } else if (this.props.authButtonStatus === "signup") {
+      dynamicbutton = (
+        <div className="authButtons">
+          <Link to="/login">
+            <button className="authButtonsLogin">Login</button>
+          </Link>
+          <Link to="/signup">
+            <button className="authButtonsSignUp">SIGN UP NOW</button>
+          </Link>
+        </div>
       );
     } else {
       dynamicbutton = (
-        <Link to="/">
-          <button className="authButtonsSignUp">CANCEL</button>
-        </Link>
+        <div className="authButtons">
+          <Link to="/login">
+            <button className="authButtonsLogin">Login</button>
+          </Link>
+          <Link to="/">
+            <button className="authButtonsSignUp">CANCEL</button>
+          </Link>
+        </div>
       );
     }
-    return (
-      <div className="authButtons">
-        <button className="authButtonsLogin">Login</button>
-        {dynamicbutton}
-      </div>
-    );
+    return <div>{dynamicbutton}</div>;
   }
 }
 
