1b53bf3f4f49eadb369f005f565551074cc3177f
Lara Petersen <laramarlenpetersen@gmail.com>
2019-04-03 18:47:27 +0200

    under construction page done

--- client/src/components/profile/EditProfile.js
+++ client/src/components/profile/EditProfile.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
+import NavBar from "../NavBar";
+import Header from "../Header";
+import Footer from "../Footer";
+import "./EditProfile.css";
 
 class EditProfile extends Component {
   render() {
     console.log(this.props.userInSession);
 
-    return <h1>HI</h1>;
+    return (
+      <div>
+        <Header
+          loggedIn={this.props.userInSession}
+          setUser={this.props.setUser}
+        />
+        <NavBar />
+
+        <div className="underConstruction">
+          <h1>Under Construction</h1>
+        </div>
+        <Footer />
+      </div>
+    );
   }
 }
 
