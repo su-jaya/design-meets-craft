14976ea085c170f051bb5684c52ed991b74a4905
Lara Petersen <laramarlenpetersen@gmail.com>
2019-04-06 13:25:35 +0200

    configured build

---
+++ server/public/precache-manifest.ed1e84973d8589559ea1eeffec3eb530.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7eb6745ea3e3bb1567f53e16d3335d5",
+    "url": "/static/media/Olympic Branding.b7eb6745.ttf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f09e03aa0c628822fd5c",
+    "url": "/static/js/main.f09e03aa.chunk.js"
+  },
+  {
+    "revision": "9afe76f07d3662088882",
+    "url": "/static/js/2.9afe76f0.chunk.js"
+  },
+  {
+    "revision": "f09e03aa0c628822fd5c",
+    "url": "/static/css/main.33d3ef99.chunk.css"
+  },
+  {
+    "revision": "9afe76f07d3662088882",
+    "url": "/static/css/2.c2af5852.chunk.css"
+  },
+  {
+    "revision": "33045db4bb950281f81ad22670ace254",
+    "url": "/index.html"
+  }
+];
