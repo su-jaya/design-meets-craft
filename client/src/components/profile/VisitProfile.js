6c772472be645b9bbd9895a799da78da3ca24be5
Susanne Tünker <susanne.tuenker@hotmail.de>
2019-05-29 14:18:34 +0200

    current status susa

--- client/src/components/profile/VisitProfile.js
+++ client/src/components/profile/VisitProfile.js
@@ -5,6 +5,9 @@ import axios from "axios";
 import ProfileCard from "./ProfileCard";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import TagsInput from "react-tagsinput";
+import Row from "react-bootstrap/Row";
 
 class VisitProfile extends Component {
   state = {
@@ -32,6 +35,8 @@ class VisitProfile extends Component {
       return "Loading...";
     }
 
+    let theUser = this.state.user;
+
     return (
       <div>
         <Header loggedIn={this.props.userInSession} />
@@ -53,21 +58,102 @@ class VisitProfile extends Component {
         </div>
 
         {/* Your Profession */}
+
         <Container fluid={true} className="profileEditContainer">
-          <div className="profileEditHeadline">
-            <h1>Your profession</h1>
-            <Link to="/editprofile">
-              <button>
-                <img
-                  className="profileEditIcon"
-                  src="/images/DmC_penicon.png"
-                  alt="pen icon"
-                  width="15em"
+          <Row>
+            <Col xs={8}>
+              <div>
+                <div className="profileEditHeadline">
+                  <h1>Your profession</h1>
+                  <Link to="/editprofile">
+                    <button>
+                      <img
+                        className="profileEditIcon"
+                        src="/images/DmC_penicon.png"
+                        alt="pen icon"
+                        width="15em"
+                      />
+                      Edit
+                    </button>
+                  </Link>
+                </div>
+                <TagsInput
+                  className="suggestedTags viewTagsOnly"
+                  id="profession"
+                  value={theUser.tagsCategory}
                 />
-                Edit
-              </button>
-            </Link>
-          </div>
+              </div>
+              <div className="homeDivider" />
+              <div>
+                <div className="profileEditHeadline">
+                  <h1>Your needs</h1>
+                </div>
+                <TagsInput
+                  className="suggestedTags viewTagsOnly"
+                  id="needs"
+                  value={theUser.tagsMaterial}
+                />
+              </div>
+              <div className="homeDivider" />
+
+              {theUser.role === "designer" ? (
+                <div>
+                  <div>
+                    <div className="profileEditHeadline">
+                      <h1>Destination</h1>
+                    </div>
+                    <TagsInput
+                      className="suggestedTags viewTagsOnly"
+                      id="destination"
+                      value={theUser.tagsDestination}
+                    />
+                  </div>
+
+                  <div className="homeDivider" />
+
+                  <div>
+                    <div className="profileEditHeadline">
+                      <h1>Production capacity</h1>
+                    </div>
+                    <p className="profileEditText">{theUser.capacity}</p>
+                  </div>
+
+                  <div className="homeDivider" />
+
+                  <div>
+                    <div className="profileEditHeadline">
+                      <h1>Looking for</h1>
+                    </div>
+                    <p className="profileEditText">{theUser.lookingfor}</p>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
+
+              {/* GALLERY SECTION  */}
+
+              {/* <div className="homeDivider" />
+              <div>
+                <div className="profileEditHeadline">
+                  <h1>Gallery</h1>
+                  <button>
+                    <img
+                      className="profileEditIcon"
+                      src="/images/DmC_penicon.png"
+                      alt="pen icon"
+                      width="15em"
+                    />
+                    Edit
+                  </button>
+                </div>
+                {theUser.gallery.map((e, idx) => (
+                  <img src={e} alt={idx} />
+                ))}
+              </div> */}
+            </Col>
+            <Col />
+          </Row>
         </Container>
       </div>
     );
